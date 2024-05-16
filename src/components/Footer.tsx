570fcb270f20079cbbd67bf21e6eb5b97fe57c49
Artur Khareksian <r2mail1122@gmail.com>
2024-05-16 17:02:30 +0300

    feat: landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import { MaxWidthWrapper } from "./MaxWidthWrapper"
+
+const Footer = () => {
+  return (
+    <MaxWidthWrapper asTag="footer" className="bg-white h-20 relative">
+      <div className="border-t border-gray-200" />
+
+      <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center text-center md:text-left pb-2 md:pb-0">
+        <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} All rights reserved</p>
+
+        <div className="flex items-center justify-center space-x-8">
+          <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+            Terms
+          </Link>
+          <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+            Privacy Policy
+          </Link>
+          <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+            Cookie Policy
+          </Link>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default Footer
